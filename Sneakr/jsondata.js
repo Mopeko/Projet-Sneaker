89af67d61808d3a99229f5894b8814b3543dc155
Mopeko <maxime.brault@epitech.digital>
2023-12-06 12:04:57 +0100

    UPT jsondata.js to new function for the dislay of sneaker

--- Sneakr/jsondata.js
+++ Sneakr/jsondata.js
@@ -1,14 +1,22 @@
-const sneakerList = document.createElement('ul');
-document.body.appendChild(sneakerList);
-
-fetch('all_data.json')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(sneaker => {
-      const listItem = document.createElement('li');
-        listItem.innerHTML = `<strong>${sneaker.attributes.brand} - ${sneaker.attributes.name}</strong> - $${sneaker.attributes.retailPrice}`;
-        sneakerList.appendChild(listItem);
-      }
-    );
-  })
-  .catch(error => console.error('Error fetching data:', error));
+function displaySneakers(data) {
+  if (data != null) {
+    let lineData = document.createElement("tr");
+    let cellData = document.createElement("td");
+    fetch('all_data.json')
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(sneaker => {
+          let sneakerTable = document.createElement("table");
+          const img = document.createElement("img")
+          img.src = sneaker.attributes.image.original
+          document.getElementById('Sneaker').appendChild(img);
+          let sneakerDiv = document.createElement("div")
+          cellData.appendChild(sneakerDiv)
+          lineData.appendChild(cellData)
+          sneakerTable.appendChild(lineData)
+          document.getElementById('Sneaker').appendChild(sneakerTable);
+        }
+        );
+      })
+  }
+}
