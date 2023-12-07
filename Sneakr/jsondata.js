20d241530817e5e4c7426c82342bfff00003849a
Mopeko <maxime.brault@epitech.digital>
2023-12-07 10:30:49 +0100

    UPT display sneaker working with norm

--- Sneakr/jsondata.js
+++ Sneakr/jsondata.js
@@ -1,22 +1,44 @@
 function displaySneakers(data) {
+  const sneakerContainer = document.getElementById('Sneaker');
+
   if (data != null) {
-    let lineData = document.createElement("tr");
-    let cellData = document.createElement("td");
-    fetch('all_data.json')
-      .then(response => response.json())
-      .then(data => {
-        data.forEach(sneaker => {
-          let sneakerTable = document.createElement("table");
-          const img = document.createElement("img")
-          img.src = sneaker.attributes.image.original
-          document.getElementById('Sneaker').appendChild(img);
-          let sneakerDiv = document.createElement("div")
-          cellData.appendChild(sneakerDiv)
-          lineData.appendChild(cellData)
-          sneakerTable.appendChild(lineData)
-          document.getElementById('Sneaker').appendChild(sneakerTable);
-        }
-        );
-      })
+    data.forEach(sneaker => {
+      const sneakerTable = document.createElement("table");
+      const lineData = document.createElement("tr");
+      const cellData = document.createElement("td");
+      let sneakerDiv = document.createElement("div");
+      const img = document.createElement("img");
+      const Name = document.createElement("p")
+      const Price = document.createElement("p")
+      let click = document.createElement("a")
+
+      
+      Price.innerHTML = sneaker.attributes.retailPrice + "€"
+      Name.innerHTML = sneaker.attributes.name
+      img.src = sneaker.attributes.image.original;
+      img.alt = sneaker.attributes.name;
+      click.appendChild(img);
+      sneakerDiv.appendChild(click)
+      sneakerDiv.appendChild(img)
+      sneakerDiv.appendChild(Price);
+      sneakerDiv.appendChild(Name);
+      cellData.appendChild(sneakerDiv);
+      lineData.appendChild(cellData);
+      sneakerTable.appendChild(lineData);
+      sneakerContainer.appendChild(sneakerTable);
+    });
   }
 }
+
+function fetchDataAndDisplay() {
+  fetch('all_data.json')
+    .then(response => response.json())
+    .then(data => {
+      displaySneakers(data);
+    })
+    .catch(error => console.error('Erreur lors de la récupération des données :', error));
+}
+fetchDataAndDisplay();
+window.onload = function() {
+  fetchDataAndDisplay();
+};
